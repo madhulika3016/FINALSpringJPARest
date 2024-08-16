dedc8658bd995774b51b08568ad4efd8d5a33048
madhulika3016 <madhulikapa@gmail.com>
2024-08-16 10:21:02 +0530

    commit

---
+++ SpringJPARESTDemo2/src/main/java/com/SpringJparestDemo2Application.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJparestDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJparestDemo2Application.class, args);
+	}
+
+}
